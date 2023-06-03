479691b6bb53a40d1ed0835165d2e9c63fed566f
Dawid Klusak <dawidklusakbusiness@gmail.com>
2023-06-03 22:08:03 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Cześć");
+
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-themeName");
+let tableDark = document.querySelector(".tableDark");
+let header = document.querySelector(".header");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("body--dark");
+
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+});
+
